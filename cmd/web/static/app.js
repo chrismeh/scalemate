9c0fc8425033ffeaa136c2b5084cd209834b8d3f
Christian Mehring <cmeh@mailbox.org>
2021-09-19 23:20:03 +0200

    Remove obsolete VueJS instantiation

--- cmd/web/static/app.js
+++ cmd/web/static/app.js
@@ -5,22 +5,3 @@ function updateImageSRC() {
 
     document.getElementById("scale-image").src = "/scale?root=" + root + "&type=" + scale + "&tuning=" + tuning;
 }
-
-const app = new Vue({
-    el: '#app',
-    data: {
-        notes: ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"],
-        scales: ["minor", "major", "harmonic minor"],
-        rootNote: "A",
-        scale: "minor",
-        tuning: "E A D G B E",
-    },
-    computed: {
-        imageSRC: function () {
-            const root = encodeURIComponent(this.rootNote)
-            const scale = encodeURIComponent(this.scale)
-            const tuning = encodeURIComponent(this.tuning)
-            return "/scale?root=" + root + "&type=" + scale + "&tuning=" + tuning;
-        },
-    }
-})
