c1517e49478c75f2727e15df8c5ce0d0596c93bf
Christian Mehring <cmeh@mailbox.org>
2021-09-19 20:22:17 +0200

    Make vanilla JS great again

--- cmd/web/static/app.js
+++ cmd/web/static/app.js
@@ -1,3 +1,11 @@
+function updateImageSRC() {
+    const root = encodeURIComponent(document.getElementById("root").value);
+    const scale = encodeURIComponent(document.getElementById("scale").value);
+    const tuning = encodeURIComponent(document.getElementById("tuning").value);
+
+    document.getElementById("scale-image").src = "/scale?root=" + root + "&type=" + scale + "&tuning=" + tuning;
+}
+
 const app = new Vue({
     el: '#app',
     data: {
