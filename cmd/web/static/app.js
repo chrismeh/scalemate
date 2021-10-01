5ab966deb490d5ef7b819ed479de918cfdcad39a
Christian Mehring <cmeh@mailbox.org>
2021-10-01 22:48:16 +0200

    Show chord selector in web application

--- cmd/web/static/app.js
+++ cmd/web/static/app.js
@@ -2,12 +2,28 @@ function updateImageSRC() {
     const root = encodeURIComponent(document.getElementById("root").value);
     const scale = encodeURIComponent(document.getElementById("scale").value);
     const tuning = encodeURIComponent(document.getElementById("tuning").value);
-    const url = "/api/scale?root=" + root + "&type=" + scale + "&tuning=" + tuning;
+    const chord = encodeURIComponent(document.getElementById("chord").value);
+    const url = "/api/scale?root=" + root + "&type=" + scale + "&tuning=" + tuning + "&chord=" + chord;
 
     fetch(url)
         .then(resp => resp.json())
         .then(json => {
-            console.log(json)
             document.getElementById("scale-image").src = `data:image/png;base64,${json.picture}`;
+
+            let chordSelect = document.getElementById("chord")
+            while (chordSelect.options.length > 0) {
+                chordSelect.remove(0);
+            }
+
+            let emptyOption = document.createElement("option");
+            emptyOption.innerHTML = "-";
+            chordSelect.appendChild(emptyOption);
+
+            for (let chord of json.chords) {
+                let opt = document.createElement("option")
+                opt.value = chord;
+                opt.innerHTML = chord;
+                chordSelect.appendChild(opt)
+            }
         })
 }
